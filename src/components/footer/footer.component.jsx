810780670136defc9641c59548af4ef9e375f5e0
luke john <luke0145555@hotmail.co.uk>
2020-08-20 20:51:51 +0100

    finished all media queries for landing page except for header

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import NewsletterForm from './newsletter-form/newsletter-form.component'
 
-import {ReactComponent as FbLogo}  from '../../assetts/icons/facebook.svg'
-import {ReactComponent as InstaLogo} from '../../assetts/icons/instagram.svg'
-import {ReactComponent as LinkedinLogo} from '../../assetts/icons/linkedin.svg'
-import {ReactComponent as TwitterLogo} from '../../assetts/icons/twitter.svg'
+import { ReactComponent as FbLogo } from '../../assetts/icons/facebook.svg'
+import { ReactComponent as InstaLogo } from '../../assetts/icons/instagram.svg'
+import { ReactComponent as LinkedinLogo } from '../../assetts/icons/linkedin.svg'
+import { ReactComponent as TwitterLogo } from '../../assetts/icons/twitter.svg'
 import './footer.styles.scss';
 
 const Footer = () => {
     return (
         <div className='footer-container'>
             <div className='footer-wrapper'>
-                <div className='footer-title-container'>
-                    <h1> CONSULT NAME </h1>
-                </div>
-                <div className='footer-address-container'>
-                    <h4 className='footer-strong-text'>Helsinki</h4>
-                    <p>Tammasaarenkatu 3</p>
-                    <p>00180 Helsinki</p>
-                    <p>Finland</p>
-                </div>
-                <div className='footer-terms-container'>
-                    <p><strong><a> Privacy Policy </a></strong></p>
-                    <p><strong><a> Terms and Conditions </a></strong></p>
+                <div className='left-footer-container'>
+                    <div className='footer-title-container'>
+                            <h1> CONSULT NAME </h1>
+                    </div>
+                    <div className='footer-text-columns'>
+                        <div className='footer-address-container'>
+                            <h4 className='footer-strong-text'>Helsinki</h4>
+                            <p>Tammasaarenkatu 3</p>
+                            <p>00180 Helsinki</p>
+                            <p>Finland</p>
+                        </div>
+                        <div className='footer-terms-container'>
+                            <p><strong><a> Privacy Policy </a></strong></p>
+                            <p><strong><a> Terms and Conditions </a></strong></p>
+                        </div>
+                    </div>
                 </div>
                 <div className='footer-newsletter-container'>
                     <h4 className='footer-strong-text'>Sign up for our newsletter</h4>
